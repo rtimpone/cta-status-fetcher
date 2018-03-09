47c5a7fe99e441b132b625907656adff15942dc4
Rob Timpone <rtimpone@gmail.com>
2018-03-09 07:57:44 -0600

    Adding new Xcode project

---
+++ CTAStatusFetcher/CTAStatusFetcher/CTAStatusFetcher.h
@@ -0,0 +1,19 @@
+//
+//  CTAStatusFetcher.h
+//  CTAStatusFetcher
+//
+//  Created by Rob Timpone on 3/9/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTAStatusFetcher.
+FOUNDATION_EXPORT double CTAStatusFetcherVersionNumber;
+
+//! Project version string for CTAStatusFetcher.
+FOUNDATION_EXPORT const unsigned char CTAStatusFetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTAStatusFetcher/PublicHeader.h>
+
+
